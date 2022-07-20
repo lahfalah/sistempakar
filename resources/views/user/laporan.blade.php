978f87f218c9687062dac2737165db0dd08899d1
Falah Insan Pratama <falahinsanpratama@Falahs-MacBook.local>
2022-07-20 08:35:01 +0700

    Laporan Cetak

--- resources/views/user/laporan.blade.php
+++ resources/views/user/laporan.blade.php
@@ -1,52 +1,39 @@
 @extends('user.layouts.main')
 @section('container')
-    <div class="card" style="background-color:#37050777">
+    <div class="card text-light" style="background-color:#37050777">
         <div class="card-header">
-            <h3 class="text-center text-light" style="">Riwayat Diagnosa</h3>
+            <h3 class="card-title">Riwayat Data Diagnosa</h3>
         </div>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <div class="card" style="background-color:#370507">
-                        <div class="card-body">
-                            <table id="example2" class="table table-bordered table-striped table-dark mb-3">
-                                <a href="/cetak" target="_blank"><button
-                                        class="btn col-sm-3 btn-info mb-3 ml-auto">Cetak Keseluruhan Data <i
-                                            class="fa-solid fa-file-pdf"></i>
+
+        <div class="card-body" style="background-color:#700a0e">
+            <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th style="width: 70px" class="text-center">No</th>
+                        <th class="text-center">Tanggal</th>
+                        <th class="text-center">Penyakit</th>
+                        <th class="text-center">Gejala</th>
+                        <th class="text-center">Cetak</th>
+                    </tr>
+                </thead>
+                @php
+                    $no = 1;
+                @endphp
+                <tbody>
+                    @foreach ($laporan as $d)
+                        <tr>
+                            <th scope="row" class="text-center">{{ $no++ }}</th>
+                            <td class="text-center">{{ $d->created_at->format('d-M-Y') }}</td>
+                            <td class="text-center">{{ $d->diagnosa->penyakit->namapenyakit }}</td>
+                            <td class="text-center">{{ count($d->diagnosa->item) }}</td>
+                            <td class="text-center"><a href="/cetak/{{ $d->id }}" target="_blank"><button
+                                        class="btn btn-success">Cetak <i class="fa-solid fa-file-pdf"></i>
                                     </button>
-                                </a>
-                                <thead>
-                                    <tr>
-                                        <th style="width: 70px" class="text-center">No</th>
-                                        <th class="text-center">Waktu</th>
-                                        <th class="text-center">Penyakit</th>
-                                        <th class="text-center">Gejala</th>
-                                        <th class="text-center">Cetak</th>
-                                    </tr>
-                                </thead>
-                                @php
-                                    $no = 1;
-                                @endphp
-                                @foreach ($laporan as $d)
-                                    <tbody>
-                                        <tr>
-                                            <th scope="row" class="text-center">{{ $no++ }}</th>
-                                            <td class="text-center">{{ $d->created_at }}</td>
-                                            <td class="text-center">{{ $d->diagnosa->penyakit->namapenyakit }}</td>
-                                            <td class="text-center">{{ count($d->diagnosa->item) }}</td>
-                                            <td class="text-center"><a href="/cetak/{{ $d->id }}" target="_blank"><button
-                                                class="btn btn-success">Cetak <i
-                                                    class="fa-solid fa-file-pdf"></i>
-                                            </button>
-                                        </a></td>
-                                        </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
-                            {{ $laporan->links() }}
-                        </div>
-                    </div>
-                </div>
-            </div>
+                                </a></td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-    @endsection
+    </div>
+@endsection
--- resources/views/user/layouts/main.blade.php
+++ resources/views/user/layouts/main.blade.php
@@ -38,6 +38,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
     
     <link rel="stylesheet" href="{{ asset('Template') }}/dist/css/adminlte.min.css?v=3.2.0">
 
+<link rel="stylesheet" href="{{ asset('Template') }}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ asset('Template') }}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="{{ asset('Template') }}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+
+<script nonce="f786ed2e-2bc3-457b-a150-da4437e56c8b">(function(w,d){!function(a,e,t,r){a.zarazData=a.zarazData||{};a.zarazData.executed=[];a.zaraz={deferred:[]};a.zaraz.q=[];a.zaraz._f=function(e){return function(){var t=Array.prototype.slice.call(arguments);a.zaraz.q.push({m:e,a:t})}};for(const e of["track","set","ecommerce","debug"])a.zaraz[e]=a.zaraz._f(e);a.zaraz.init=()=>{var t=e.getElementsByTagName(r)[0],z=e.createElement(r),n=e.getElementsByTagName("title")[0];n&&(a.zarazData.t=e.getElementsByTagName("title")[0].text);a.zarazData.x=Math.random();a.zarazData.w=a.screen.width;a.zarazData.h=a.screen.height;a.zarazData.j=a.innerHeight;a.zarazData.e=a.innerWidth;a.zarazData.l=a.location.href;a.zarazData.r=e.referrer;a.zarazData.k=a.screen.colorDepth;a.zarazData.n=e.characterSet;a.zarazData.o=(new Date).getTimezoneOffset();a.zarazData.q=[];for(;a.zaraz.q.length;){const e=a.zaraz.q.shift();a.zarazData.q.push(e)}z.defer=!0;for(const e of[localStorage,sessionStorage])Object.keys(e||{}).filter((a=>a.startsWith("_zaraz_"))).forEach((t=>{try{a.zarazData["z_"+t.slice(7)]=JSON.parse(e.getItem(t))}catch{a.zarazData["z_"+t.slice(7)]=e.getItem(t)}}));z.referrerPolicy="origin";z.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(a.zarazData)));t.parentNode.insertBefore(z,t)};["complete","interactive"].includes(e.readyState)?zaraz.init():a.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,0,"script");})(window,document);</script></head>
+
 
     <!-- jQuery -->
     <script src="{{ asset('Template') }}/plugins/jquery/jquery.min.js"></script>
@@ -68,7 +74,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <script src="{{ asset('Template') }}/plugins/dropzone/min/dropzone.min.js"></script>
 
 <script src="{{ asset('Template') }}/dist/js/adminlte.min.js?v=3.2.0"></script>
-
+<script src="{{ asset('Template') }}/plugins/jquery/jquery.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/jszip/jszip.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="{{ asset('Template') }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.4.20/dist/sweetalert2.all.min.js"></script>
     
 </head>
@@ -109,6 +127,23 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src="https://kit.fontawesome.com/f259651f11.js" crossorigin="anonymous"></script>
 </body>
 
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
 <script>
     $(function () {
       //Initialize Select2 Elements
